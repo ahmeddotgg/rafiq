4a68da1cc8e26c2f05e7069a4b591088de2619cb
Ahmed Said <ahmeddsaidd@ptoton.me>
2025-12-10 15:18:47 +0200

    improve handlers

--- server/src/modules/jobs/index.ts
+++ server/src/modules/jobs/index.ts
@@ -1,6 +1,22 @@
-import Elysia from 'elysia';
+import Elysia, { t } from 'elysia';
 import { JobsServices } from './service';
 
 export const jobs = new Elysia({ prefix: '/jobs' });
 
-jobs.get('/', async () => await JobsServices.all());
+jobs.get('/', async () => {
+  const jobs = await JobsServices.all();
+  return { info: 'success', data: jobs };
+});
+
+jobs.get(
+  '/:id',
+  async ({ params: { id } }) => {
+    const job = await JobsServices.find(id);
+    return { info: 'success', data: job };
+  },
+  {
+    params: t.Object({
+      id: t.String()
+    })
+  }
+);
