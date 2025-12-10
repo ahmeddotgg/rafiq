8036e70e45fe86fce96c8c0e725dbb02285c24b7
Ahmed Said <ahmeddsaidd@ptoton.me>
2025-12-10 13:54:44 +0200

    base setup

---
+++ client/src/index.tsx
@@ -0,0 +1,27 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { routeTree } from './routeTree.gen.ts';
+import './index.css';
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  scrollRestoration: true
+});
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
