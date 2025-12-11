616753ae2a53b46b78911d400a5268d9d72c252f
Ahmed Said <ahmeddsaidd@ptoton.me>
2025-12-11 20:20:00 +0200

    initialise dashbaord route

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
+import { buttonVariants } from '@/components/ui/button';
 
 export const Route = createFileRoute('/')({
   component: RouteComponent
 });
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>;
+  return (
+    <div>
+      <Link to="/dashboard" className={buttonVariants()}>
+        Dashboard
+      </Link>
+    </div>
+  );
 }
